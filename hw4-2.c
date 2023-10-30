891a51db21240fe15065ac60672b3a8cb64ee217
zheng0225 <146065408+zheng0225@users.noreply.github.com>
2023-10-30 19:15:42 +0800

    Update hw4-2.c

--- hw4-2.c
+++ hw4-2.c
@@ -1,1 +1,70 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main(){
+    int a,b;
 
+
+
+
+        scanf("%d%d",&a,&b);
+        if (a!=123&&a!=456&&a!=789&&a!=336&&a!=775&&a!=566)
+        {
+
+         printf("error");}
+
+         if (a==123)
+         {if(b==456)
+             printf("9000");
+
+         else{
+                printf("error");
+
+         }}
+          if (a==456)
+         {if(b==789)
+             printf("5000");
+
+         else{
+                printf("error");
+
+         }}
+          if (a==789)
+         {if (b==888)
+             printf("6000");
+
+         else{
+                printf("error");
+
+         }}
+          if (a==336)
+         {if(b==558)
+             printf("10000");
+
+         else{
+                printf("error");
+
+         }}
+          if (a==775)
+         {if(b==666)
+             printf("12000");
+
+         else{
+                printf("error");
+
+         }}
+          if (a==566)
+         {if(b==221)
+             printf("7000");
+
+         else{
+                printf("error");
+
+         }}
+
+
+
+
+
+
+
+}
